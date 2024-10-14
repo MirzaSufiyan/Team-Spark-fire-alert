89f7ba2714fe253a5c93b4c6efc48ba45fbb028d
coding champion <codingchampionofficial@gmail.com>
2024-10-14 22:48:05 +0530

    Update login.java

--- src/main/java/MyPackage/login.java
+++ src/main/java/MyPackage/login.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-@WebServlet("/login")
+// @WebServlet("/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
